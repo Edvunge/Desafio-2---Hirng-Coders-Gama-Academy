ed236d4af7a1f95a6b8672142be8fc1051a2ce02
edvunge <edvaldolaurentina07@hotmail.com>
2021-07-23 23:57:48 +0100

    7th - Seventh

---
+++ assents/index.js
@@ -0,0 +1,34 @@
+
+// Apresentação de slides automática - mudar a imagem a cada 4 segundos
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 4000);    
+}
+
+// Usado para alternar o menu em telas pequenas ao clicar no botão de menu
+function myFunction() {
+  var x = document.getElementById("navDemo");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+
+// Quando o usuário clicar em qualquer lugar fora do modal, feche-o
+var modal = document.getElementById('ticketModal');
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
